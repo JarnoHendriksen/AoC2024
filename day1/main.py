4f084ea7247f500a58b65eff1e0baa19f570e40d
JarnoHendriksen <jarno.hendriksen@gmail.com>
2024-12-02 11:57:45 +0100

    refactoring

--- day1/main.py
+++ day1/main.py
@@ -1,8 +1,6 @@
 import numpy as np
 
-listpair = tuple[np.ndarray[int], np.ndarray[int]]
-
-def read_input(path: str) -> listpair:
+def read_input(path: str) -> tuple[list[int], list[int]]:
     l1 = []
     l2 = []
     with open(path, 'r') as file:
@@ -12,12 +10,6 @@ def read_input(path: str) -> listpair:
             l2.append(int(split_string[1]))
     
     return (np.array(l1), np.array(l2))
-
-def sort_lists(l: listpair) -> listpair:
-    return (np.sort(l[0]), np.sort(l[1]))
-
-def get_distances(l: listpair) -> np.ndarray[int]:
-    return np.absolute(l[0] - l[1])
 
 def count_occurrences(l2: list[int]) -> dict[int, int]:
     sums = {}
@@ -30,21 +22,21 @@ def count_occurrences(l2: list[int]) -> dict[int, int]:
     
     return sums
 
-def get_similarity_score(l: listpair) -> int:
+def get_similarity_score(l1: list[int], l2: list[int]) -> int:
     sum = 0
-    l2_occurences = count_occurrences(l[1])
+    l2_occurences = count_occurrences(l2)
 
-    for i in l[0]:
+    for i in l1:
         sum += i * l2_occurences.get(i, 0)
 
     return sum
 
 
 if __name__ == '__main__':
-    input_lists = read_input('input.txt')
-    sorted_lists = sort_lists(input_lists)
-    dists = get_distances(sorted_lists)
+    l1, l2 = read_input('input.txt')
+    l1, l2 = (np.sort(l1), np.sort(l2))
+    dists = np.absolute(l1 - l2)
     result1 = np.sum(dists)
     print(f"Result part 1: {result1}")
-    result2 = get_similarity_score(sorted_lists)
+    result2 = get_similarity_score(l1, l2)
     print(f"Result part 2: {result2}")
