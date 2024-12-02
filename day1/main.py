d337f215753bbf0fa13d605c310e353f0ab07577
JarnoHendriksen <jarno.hendriksen@gmail.com>
2024-12-02 11:53:18 +0100

    day 1, part 2

--- day1/main.py
+++ day1/main.py
@@ -19,9 +19,32 @@ def sort_lists(l: listpair) -> listpair:
 def get_distances(l: listpair) -> np.ndarray[int]:
     return np.absolute(l[0] - l[1])
 
+def count_occurrences(l2: list[int]) -> dict[int, int]:
+    sums = {}
+
+    for i in l2:
+        if not i in sums:
+            sums[i] = 1
+        else:
+            sums[i] += 1
+    
+    return sums
+
+def get_similarity_score(l: listpair) -> int:
+    sum = 0
+    l2_occurences = count_occurrences(l[1])
+
+    for i in l[0]:
+        sum += i * l2_occurences.get(i, 0)
+
+    return sum
+
+
 if __name__ == '__main__':
     input_lists = read_input('input.txt')
     sorted_lists = sort_lists(input_lists)
     dists = get_distances(sorted_lists)
-    result = np.sum(dists)
-    print(f"Result: {result}")
+    result1 = np.sum(dists)
+    print(f"Result part 1: {result1}")
+    result2 = get_similarity_score(sorted_lists)
+    print(f"Result part 2: {result2}")
